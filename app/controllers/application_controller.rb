b07bef02afe2131cb65ce67bd602a8813c44803d
Vladimir Masko <soupdawgie@gmail.com>
2016-05-02 13:35:23 +0300

    Add gravatar, devise helpers, render flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_sanitized_params, if: :devise_controller?
+
+  protected
+
+  def configure_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :name, :picture, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :name, :picture, :password, :password_confirmation,
+                                                   :current_password)
+    end
+  end
 end
---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,17 @@
+module DeviseHelper
+
+  def bootstrap_devise_error_messages!
+    if resource.errors.any?
+      messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+      count = pluralize(resource.errors.count, "error")
+      html = <<-HTML
+      <div class="alert alert-danger">
+        The form contains #{count}:
+        #{messages}
+      </div>
+      HTML
+      html.html_safe
+    end
+  end
+
+end
