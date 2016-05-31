10dfd61e6c6c669290ca8942575b1edfe3b82998
Oleg Bovykin <oleg.bovykin@gmail.com>
2016-05-31 13:02:36 +0300

    Добавил роли

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable, :confirmable, :trackable
   mount_uploader :avatar, AvatarUploader
   validates_processing_of :avatar
   validate :avatar_size_validation
@@ -11,5 +11,4 @@ class User < ApplicationRecord
   def avatar_size_validation
     errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
   end
-
 end
