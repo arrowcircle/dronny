0a411989e3f6e7e92fac978286f95ce9d9212343
Oleg Bovykin <oleg.bovykin@gmail.com>
2016-06-21 17:58:48 +0300

    Обновил гемы

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -3,6 +3,6 @@ require 'full_helper'
 feature 'Главная страница' do
   scenario 'Открывается' do
     visit root_url
-    expect(page).to have_content 'Dronny'
+    expect(page).to have_content 'Дронни'
   end
 end
